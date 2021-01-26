e1f54697a74c7eef15a25b34b1199bd87356fe25
PetroPinchuk <locator862@gmail.com>
2021-01-26 22:02:07 +0100

    created functional of automatic opening and closing of doors, and movement up and down; created a current floor display

--- js/script.js
+++ js/script.js
@@ -1,35 +1,3 @@
-// ------------Відкривання дверей--------------
-document.querySelector('#open-door').onclick = function () {
-  document.querySelector('.right-door').style.marginLeft = '120px';
-}
-// ------------Закривання дверей--------------
-document.querySelector('#close-door').onclick = function () {
-  document.querySelector('.right-door').style.marginLeft = '0';
-}
-
-
-
-// ------------1 поверх--------------
-document.querySelector('#first').onclick = function () {
-  document.querySelector('.elevator').style.marginBottom = 17 + 'px';
-}
-// ------------2 поверх--------------
-document.querySelector('#two').onclick = function () {
-  document.querySelector('.elevator').style.marginBottom = 226 + 'px';
-}
-// ------------3 поверх--------------
-document.querySelector('#third').onclick = function () {
-  document.querySelector('.elevator').style.marginBottom = 433 + 'px';
-}
-// ------------4 поверх--------------
-document.querySelector('#four').onclick = function () {
-  document.querySelector('.elevator').style.marginBottom = 638 + 'px';
-}
-// ------------5 поверх--------------
-document.querySelector('#five').onclick = function () {
-  document.querySelector('.elevator').style.marginBottom = 843 + 'px';
-}
-
 // ------------1 поверх--------------
 document.querySelector('#oneButton').onclick = function () {
   document.querySelector('.elevator').style.marginBottom = 17 + 'px';
@@ -50,3 +18,67 @@ document.querySelector('#fourButton').onclick = function () {
 document.querySelector('#fiveButton').onclick = function () {
   document.querySelector('.elevator').style.marginBottom = 843 + 'px';
 }
+
+
+// ------------Відкривання дверей--------------
+document.querySelector('#open-door').onclick = function () {
+  document.querySelector('.right-door').style.marginLeft = '120px';
+}
+// ------------Закривання дверей--------------
+document.querySelector('#close-door').onclick = function () {
+  document.querySelector('.right-door').style.marginLeft = '0';
+}
+
+// ----------------------------------------------------------------
+
+
+let elevator = document.querySelector('.elevator');
+let buttonFromPanel = document.querySelector('.button-block');
+
+function openTheDoor() {
+  document.querySelector('.right-door').style.marginLeft = '120px';
+  return true;
+}
+function closeTheDoor() {
+  document.querySelector('.right-door').style.marginLeft = '0';
+  return true;
+}
+function moveUpMoveDown(x) {
+  elevator.style.marginBottom = x + 'px';
+}
+
+
+buttonFromPanel.onclick = function (e) {
+  console.log(e.target.id);
+  if (e.target.id == 'first') {
+    closeTheDoor();
+    setTimeout(function() {
+        moveUpMoveDown(17);
+    }, 2000);
+    setTimeout(openTheDoor, 4000);
+  } else if (e.target.id == 'two') {
+    closeTheDoor();
+    setTimeout(function() {
+        moveUpMoveDown(226);
+    }, 2000);
+    setTimeout(openTheDoor, 4000);
+  } else if (e.target.id == 'third') {
+    closeTheDoor();
+    setTimeout(function() {
+        moveUpMoveDown(433);
+    }, 2000);
+    setTimeout(openTheDoor, 4000);
+  } else if (e.target.id == 'four') {
+    closeTheDoor();
+    setTimeout(function() {
+        moveUpMoveDown(638);
+    }, 2000);
+    setTimeout(openTheDoor, 4000);
+  } else if (e.target.id == 'five') {
+    closeTheDoor();
+    setTimeout(function() {
+        moveUpMoveDown(843);
+    }, 2000);
+    setTimeout(openTheDoor, 4000);
+  }
+};
